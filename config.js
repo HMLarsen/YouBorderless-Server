cbe741a02d2ad12dde92fb713bee5872d7d6f11d
HMLarsen <hugomarcel91@gmail.com>
2021-04-24 18:26:34 -0300

    change project

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 module.exports = {
 	NODE_ENV: process.env.NODE_ENV || 'development',
 	PORT: process.env.PORT || 3000,
-	CORS_ORIGIN: process.env.NODE_ENV === undefined ? '*' : 'https://youborderless-d0a95.web.app',
-	GOOGLE_PROJECT_ID: 'dulcet-pilot-307823',
+	CORS_ORIGIN: process.env.NODE_ENV === undefined ? '*' : 'https://youborderless-42519.web.app',
 	YTDL_BINARY_NAME: process.env.NODE_ENV === undefined ? 'youtube-dl.bin' : 'youtube-dl-linux.bin',
 }
--- google/translation/translation.service.js
+++ google/translation/translation.service.js
@@ -1,30 +1,4 @@
-const { TranslationServiceClient } = require('@google-cloud/translate');
 const translate = require('google-translate-open-api').default;
-const config = require('../../config.js');
-
-const translationClient = new TranslationServiceClient();
-const projectId = config.GOOGLE_PROJECT_ID;
-const location = 'global';
-
-/**
- * Parameters to configure the request can be found at:
- * https://cloud.google.com/translate/docs/reference/rest/v3/projects/translateText
- *
- * @param liveOptions options to translate
- * @param text words to translate
- * @returns
- */
-async function translateTextCharge(liveOptions, text) {
-	const request = {
-		parent: `projects/${projectId}/locations/${location}`,
-		contents: [text],
-		mimeType: 'text/plain',
-		sourceLanguageCode: liveOptions.liveLanguage.bcp,
-		targetLanguageCode: liveOptions.liveToLanguage.code
-	};
-	const [response] = await translationClient.translateText(request);
-	return response.translations[0].translatedText;
-}
 
 function translateTextFree(liveOptions, text) {
 	return translate(text, {
@@ -33,5 +7,4 @@ function translateTextFree(liveOptions, text) {
 	});
 }
 
-exports.translateTextCharge = translateTextCharge;
 exports.translateTextFree = translateTextFree;
