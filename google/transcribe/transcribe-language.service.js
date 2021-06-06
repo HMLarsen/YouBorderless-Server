fe581bda3c20083d3cc44828bd77943a2e66f54f
HMLarsen <hugomarcel91@gmail.com>
2021-06-06 14:24:12 -0300

    fix for paper

--- google/transcribe/transcribe-language.service.js
+++ google/transcribe/transcribe-language.service.js
@@ -81,7 +81,7 @@ function configureRequestToRecognize(liveOptions) {
 			encoding: 'FLAC', // optional field for FLAC or WAV files because it is in the header
 			sampleRateHertz: 48000, // optional field for FLAC or WAV files because it is in the header
 			languageCode: liveOptions.liveLanguage.bcp,
-			useEnhanced: true, // if true and the model does not support, api changes to standard version of the specified model
+			useEnhanced: true, // if true and the model does not support, api changes to standard version
 			enableAutomaticPunctuation: !!liveOptions.punctuation,
 			profanityFilter: !!liveOptions.profanityFilter,
 			//enableWordTimeOffsets: true
